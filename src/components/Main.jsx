74eb40a4ec0e71fda39e9b82d049d9ddc858cc77
VoilaSecari <lifeaslaurafilip@gmail.com>
2025-05-21 21:50:15 +0200

    prova prop

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,50 @@
-export default function Main() {
+export default function Main(props) {
   return (
     <main>
       <div className="content">
         <div className="container">
-          <h2>Il tuo contenuto va qua</h2>
+          <h2>Il tuo contenuto va qua, che è {props.content}</h2>
         </div>
       </div>
-      <div className="options"></div>
+      <div className="options">
+        <div className="buy-options">
+          <div className="options-button">
+            <img
+              src="src/assets/img/buy-comics-digital-comics.png"
+              alt="DIGITAL COMICS"
+            />
+            <h4>DIGITAL COMICS</h4>
+          </div>
+          <div className="options-button">
+            <img
+              src="src/assets/img/buy-comics-digital-comics.png"
+              alt="DIGITAL COMICS"
+            />
+            <h4>DIGITAL COMICS</h4>
+          </div>
+          <div className="options-button">
+            <img
+              src="src/assets/img/buy-comics-digital-comics.png"
+              alt="DIGITAL COMICS"
+            />
+            <h4>DIGITAL COMICS</h4>
+          </div>
+          <div className="options-button">
+            <img
+              src="src/assets/img/buy-comics-digital-comics.png"
+              alt="DIGITAL COMICS"
+            />
+            <h4>DIGITAL COMICS</h4>
+          </div>
+          <div className="options-button">
+            <img
+              src="src/assets/img/buy-comics-digital-comics.png"
+              alt="DIGITAL COMICS"
+            />
+            <h4>DIGITAL COMICS</h4>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
