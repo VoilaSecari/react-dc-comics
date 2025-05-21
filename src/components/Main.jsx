d80b43d592c5692c38610934e2bc7caba416e8e3
VoilaSecari <lifeaslaurafilip@gmail.com>
2025-05-21 16:28:50 +0200

    sistemato header, iniziato main

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+export default function Main() {
+  return (
+    <main>
+      <div className="content">
+        <div className="container">
+          <h2>Il tuo contenuto va qua</h2>
+        </div>
+      </div>
+      <div className="options"></div>
+    </main>
+  );
+}
